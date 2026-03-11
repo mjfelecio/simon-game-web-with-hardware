6b8e26fb72f27055d2b753041a06b23bb719ed28
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2026-03-11 21:58:19 +0800

    feat: setup fonts and custom colors

--- src/features/play/components/SimonButton.tsx
+++ src/features/play/components/SimonButton.tsx
@@ -8,12 +8,11 @@ type Props = {
 };
 
 const BUTTON_COLOR: Record<ButtonType, string> = {
-  blue: "bg-blue-500",
-  red: "bg-red-500",
-  green: "bg-green-500",
-  yellow: "bg-yellow-500",
+  red: "bg-simon-red",
+  green: "bg-simon-green",
+  blue: "bg-simon-blue",
+  yellow: "bg-simon-yellow",
 };
-
 
 const SimonButton = ({ type, isActive, isDisabled, onClick }: Props) => {
   return (
@@ -21,9 +20,8 @@ const SimonButton = ({ type, isActive, isDisabled, onClick }: Props) => {
       disabled={isDisabled}
       onClick={() => onClick(type)}
       type="button"
-      className={`${BUTTON_COLOR[type]} ${isActive ? "opacity-100" : "opacity-40"} transition-all relative size-24 not-disabled:opacity-100 not-disabled:active:scale-105 rounded-2xl cursor-pointer`}
-    >
-    </button>
+      className={`${BUTTON_COLOR[type]} ${isActive ? "opacity-100" : "opacity-40"} font-inter transition-all relative size-24 not-disabled:opacity-100 not-disabled:active:scale-105 rounded-2xl cursor-pointer`}
+    ></button>
   );
 };
 
