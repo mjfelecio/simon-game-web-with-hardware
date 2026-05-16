7001ecae7f2997cd046c175eee8397ac41217528
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2026-05-16 15:55:30 +0800

    deps: remove dexie as a dependency and other related code

--- src/globals/libs/db.ts
+++ src/globals/libs/db.ts
@@ -1,21 +1,5 @@
-import Dexie, { type EntityTable } from 'dexie';
-import type { Score } from '@/globals/types/simon';
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from "@/globals/types/database"
-
-export class SimonDatabase extends Dexie {
-  scores!: EntityTable<Score, 'id'>;
-
-  constructor() {
-    super("SimonGameDB");
-    
-    this.version(1).stores({
-      scores: '++id, score, level, mode, achievedAt'
-    });
-  }
-}
-
-export const db = new SimonDatabase();
 
 const API_URL = import.meta.env.VITE_SUPABASE_URL
 const PUB_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
