b23d6bbd9bc98c5461bced7422e33a6ec0a6ad3e
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2026-05-28 21:13:38 +0800

    feat: add and register new sfx

--- src/features/audio/constants/sfx.ts
+++ src/features/audio/constants/sfx.ts
@@ -1,7 +1,24 @@
 import BTN_HOVER from "@/globals/assets/audio/sfx/hover-btn.mp3";
-import BTN_CLICK from "@/globals/assets/audio/sfx/hover-btn.mp3";
+import BTN_CLICK from "@/globals/assets/audio/sfx/select.wav";
+
+import AWESOME from "@/globals/assets/audio/sfx/awesome.wav";
+import EXCELLENT from "@/globals/assets/audio/sfx/excellent.wav";
+import WONDERFUL from "@/globals/assets/audio/sfx/wonderful.wav";
+
+import BEGIN from "@/globals/assets/audio/sfx/begin.wav";
+import OVER from "@/globals/assets/audio/sfx/over.wav";
 
 export const SFX = {
+	// BUTTONS
 	BTN_HOVER,
-	BTN_CLICK
+	BTN_CLICK,
+
+	// MARKERS
+	BEGIN,
+	OVER,
+
+	// APPLAUSE
+	AWESOME,
+	EXCELLENT,
+	WONDERFUL
 } as const
