b7e200babdd149e0121abb01fd4a74c8f0fa53fe
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2026-06-06 17:18:47 +0800

    chore: add DevOverlay for testing

---
+++ src/globals/components/DevOverlay.tsx
@@ -0,0 +1,14 @@
+import Button from './Button'
+
+const DevOverlay = () => {
+	return (
+		<div className='fixed bottom-5 right-5'>
+			<Button
+				text="Test"
+				onClick={() => {}}
+			/>
+		</div>
+	)
+}
+
+export default DevOverlay
